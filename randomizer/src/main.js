720d549f0eaa15c811daef27d7f33290d287cb31
Dana Bida <42296148+DanaBida@users.noreply.github.com>
2026-01-09 19:13:35 +0200

    Fix Sentry import and middleware

--- randomizer/src/main.js
+++ randomizer/src/main.js
@@ -3,7 +3,7 @@ import { errorMiddleware } from './middlewares/error.middleware.js';
 import { config } from './config.js';
 import express from 'express';
 import cors from 'cors
-import * as Sentry from '@sentry/node';';
+import * as Sentry from '@sentry/node';
 
 const app = express();
 const { port } = confi
@@ -16,17 +16,17 @@ Sentry.init({
 });
 
 // Sentry request handler must be the first middleware
-app.use(Sentry.Handlers.requestHandler());g;
+app.use(Sentry.Handlers.requestHandler());
 
 (async () => {
   app.use(cors());
 
+app.use(appController);
 // Sentry error handler must be before any other error middleware
 app.use(Sentry.Handlers.errorHandler());
-  app.use(appController);
-  app.use(errorMiddleware);
+app.use(errorMiddleware);
 
-  app.listen(port, () => {
-    console.log(`Listening on ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
 })();
