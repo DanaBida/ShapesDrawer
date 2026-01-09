7a8ba65c8ce2983fdd2fea3fe6d8a31b1094d16b
Dana Bida <42296148+DanaBida@users.noreply.github.com>
2026-01-09 19:17:09 +0200

    Add test error function for Sentry backend testing
    Added a test method to simulate a Sentry error.

--- randomizer/src/services/colors.service.js
+++ randomizer/src/services/colors.service.js
@@ -3,7 +3,11 @@ import { randomElemFromArr } from '../utils/random.js';
 export class ColorsService {
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-  getColor = async () => {
+  getColor = async () =>
+
+  testSentryError = () => {
+    throw new Error('Backend test error - Sentry is working!');
+  }; {
     return randomElemFromArr(this.colors);
   };
 }
