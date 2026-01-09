5c56feea6b9b00d78628a10f5dd47d5a9b426218
Dana Bida <42296148+DanaBida@users.noreply.github.com>
2026-01-09 19:21:30 +0200

    Refactor getColor method to include error test

--- randomizer/src/services/colors.service.js
+++ randomizer/src/services/colors.service.js
@@ -2,12 +2,12 @@ import { randomElemFromArr } from '../utils/random.js';
 
 export class ColorsService {
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
-  getColor = async () =>
-
-  testSentryError = () => {
-    throw new Error('Backend test error - Sentry is working!');
-  }; {
-    return randomElemFromArr(this.colors);
-  };
+  getColor = async () => {
+    const testSentryError = () => {
+      throw new Error('Backend test error - Sentry is working!');
+    };
+    const result = randomElemFromArr(this.colors);
+    testSentryError()
+    return result
+  }
 }
